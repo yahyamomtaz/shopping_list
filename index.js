90c6aae5dc3b8f850a59dc0a1c8b3dd2994aa92b
yahyamomtaz <yahyamomtaz@gmail.com>
2024-01-22 14:30:03 +0100

    databse added

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
 
 appSettings = {
-    databaseURL: "https://shoppinglist-fbbd6-default-rtdb.europe-west1.firebasedatabase.app"
+    databaseURL: "https://shoppinglist-e7b69-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-const app = initializeApp(appSettings)
+const firebaseConfig = {
+    apiKey: "AIzaSyAsCudbRmuRnNzlbBZbUAFUef06K0R3sY8",
+    authDomain: "shoppinglist-e7b69.firebaseapp.com",
+    databaseURL: "https://shoppinglist-e7b69-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "shoppinglist-e7b69",
+    storageBucket: "shoppinglist-e7b69.appspot.com",
+    messagingSenderId: "574732154234",
+    appId: "1:574732154234:web:37e12d7a5a755de26d12c3"
+  };
+
+const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
-const itemsInDB = ref(database, 'items')
+const shoppingListInDB = ref(database, "shoppingList")
 
-let addButtonEl = document.getElementById('add-button')
-let inputFieldEl = document.getElementById('input-field')
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
 
-addButtonEl.addEventListener('click', function () {
-    let inputFieldValue = inputFieldEl.value
-    push(itemsInDB, {
-        name: inputFieldValue
-    })
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    console.log(inputValue)
 })
