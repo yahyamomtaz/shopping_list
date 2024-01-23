6d8750023ff30de4e3aeb3242932f7c45ce0329d
yahyamomtaz <yahyamomtaz@gmail.com>
2024-01-23 23:00:59 +0100

    database fixed

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
 
-appSettings = {
-    databaseURL: "https://shoppinglist-e7b69-default-rtdb.europe-west1.firebasedatabase.app/"
+const appSettings = {
+    databaseURL: "https://shoppinglist-e7b69-default-rtdb.europe-west1.firebasedatabase.app"
 }
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAsCudbRmuRnNzlbBZbUAFUef06K0R3sY8",
-    authDomain: "shoppinglist-e7b69.firebaseapp.com",
-    databaseURL: "https://shoppinglist-e7b69-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "shoppinglist-e7b69",
-    storageBucket: "shoppinglist-e7b69.appspot.com",
-    messagingSenderId: "574732154234",
-    appId: "1:574732154234:web:37e12d7a5a755de26d12c3"
-  };
-
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
+  
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+function clearinputFieldEl () {
+    inputFieldEl.value = ""
+}
 
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
+  
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    push(shoppingListInDB, inputValue)
-    
-    console.log(inputValue)
-})
+
+      let inputValue = inputFieldEl.value;
+      
+      push(shoppingListInDB, inputValue);
+
+      appendItemToShoppingListEl(inputValue);
+      clearinputFieldEl()
+  });
