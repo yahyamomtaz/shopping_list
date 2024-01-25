6fdda5f708c4aa691da66aeba02b274caf7d2cb9
yahyamomtaz <yahyamomtaz@gmail.com>
2024-01-25 10:34:04 +0100

    new element created

--- index.js
+++ index.js
@@ -14,10 +14,16 @@ const addButtonEl = document.getElementById("add-button");
 const shoppingListEl = document.getElementById("shopping-list");
 
 onValue(shoppingListInDB, function(snapshot) {
-    let itemsArray = Object.values(snapshot.val())
+
+    let itemsArray = Object.entries(snapshot.val())
     clearShoppingListEl()
+
     for (let i=0; i<itemsArray.length; i++) {
-        appendItemToShoppingListEl(itemsArray[i])
+
+        let currentItem = itemsArray[i]
+        let currentItemID = currentItem[0]
+        let currentItemValue = currentItem[1]
+        appendItemToShoppingListEl(currentItem)
     }
 })
 
@@ -29,8 +35,16 @@ function clearinputFieldEl() {
     inputFieldEl.value = ""
 }
 
-function appendItemToShoppingListEl(itemValue) {
-    shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+function appendItemToShoppingListEl(item) {
+    
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    shoppingListEl.append(newEl)
 }
   
 addButtonEl.addEventListener("click", function() {
