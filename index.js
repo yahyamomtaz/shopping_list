05622e25e19dc7fda0a3294bd26fd8a2447a5c74
yahyamomtaz <yahyamomtaz@gmail.com>
2024-01-25 23:20:09 +0100

    view port added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://shoppinglist-e7b69-default-rtdb.europe-west1.firebasedatabase.app"
@@ -15,16 +15,23 @@ const shoppingListEl = document.getElementById("shopping-list");
 
 onValue(shoppingListInDB, function(snapshot) {
 
-    let itemsArray = Object.entries(snapshot.val())
-    clearShoppingListEl()
+    if (snapshot.exists()) {
 
-    for (let i=0; i<itemsArray.length; i++) {
+        let itemsArray = Object.entries(snapshot.val())
+        clearShoppingListEl()
+    
+        for (let i=0; i<itemsArray.length; i++) {
+    
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            appendItemToShoppingListEl(currentItem)
+        }
 
-        let currentItem = itemsArray[i]
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItem[1]
-        appendItemToShoppingListEl(currentItem)
+    } else {
+        shoppingListEl.innerHTML = "No items in the shopping list..."
     }
+  
 })
 
 function clearShoppingListEl() {
@@ -43,6 +50,12 @@ function appendItemToShoppingListEl(item) {
     let newEl = document.createElement("li")
     
     newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function() {
+         
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        remove(exactLocationOfItemInDB)
+    })
     
     shoppingListEl.append(newEl)
 }
